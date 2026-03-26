1c39cc5f8205e9c561fbeaa2208fd8f39d2765de
Mitchell Bagot <packof14eggies@gmail.com>
2026-03-26 16:10:53 +1100

    primitives: Move script module errors to error submodule
    At present, some modules in primitives separate error types into
    submodules with re-exports while others simply define the error types
    in the module. This inconsistency is confusing and makes the docs vary.
    All error types for each top-level module should be moved into a
    relevant submodule and re-exported without inline docs.
    
    Move error types from script module into error submodule and re-export.

--- primitives/src/script/owned.rs
+++ primitives/src/script/owned.rs
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: CC0-1.0
 
-use core::convert::Infallible;
-use core::fmt;
 use core::marker::PhantomData;
 use core::ops::{Deref, DerefMut};
 
 #[cfg(feature = "arbitrary")]
 use arbitrary::{Arbitrary, Unstructured};
-use encoding::{ByteVecDecoder, ByteVecDecoderError};
-use internals::write_err;
+use encoding::ByteVecDecoder;
 
-use super::Script;
 #[cfg(feature = "hex")]
-use crate::hex;
+use super::error::FromHexError;
+use super::{Script, ScriptBufDecoderError};
 use crate::prelude::{Box, Vec};
 
 /// An owned, growable script.
@@ -217,70 +214,6 @@ impl<T> encoding::Decoder for ScriptBufDecoder<T> {
 impl<T> encoding::Decodable for ScriptBuf<T> {
     type Decoder = ScriptBufDecoder<T>;
     fn decoder() -> Self::Decoder { ScriptBufDecoder(ByteVecDecoder::new(), PhantomData) }
-}
-
-/// An error consensus decoding a `ScriptBuf<T>`.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ScriptBufDecoderError(ByteVecDecoderError);
-
-impl From<Infallible> for ScriptBufDecoderError {
-    fn from(never: Infallible) -> Self { match never {} }
-}
-
-impl fmt::Display for ScriptBufDecoderError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write_err!(f, "decoder error"; self.0) }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for ScriptBufDecoderError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> { Some(&self.0) }
-}
-
-/// An error parsing a script from hex.
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-#[cfg(feature = "hex")]
-pub enum FromHexError {
-    /// Error parsing the hex input string.
-    Hex(hex::DecodeVariableLengthBytesError),
-    /// Error when decoding the script.
-    Decoder(encoding::DecodeError<ScriptBufDecoderError>),
-}
-
-#[cfg(feature = "hex")]
-impl From<Infallible> for FromHexError {
-    fn from(never: Infallible) -> Self { match never {} }
-}
-
-#[cfg(feature = "hex")]
-impl fmt::Display for FromHexError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Self::Hex(ref e) => write_err!(f, "script hex"; e),
-            Self::Decoder(ref e) => write_err!(f, "script decoder"; e),
-        }
-    }
-}
-
-#[cfg(feature = "hex")]
-#[cfg(feature = "std")]
-impl std::error::Error for FromHexError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-            Self::Hex(ref e) => Some(e),
-            Self::Decoder(ref e) => Some(e),
-        }
-    }
-}
-
-#[cfg(feature = "hex")]
-impl From<hex::DecodeVariableLengthBytesError> for FromHexError {
-    fn from(e: hex::DecodeVariableLengthBytesError) -> Self { Self::Hex(e) }
-}
-
-#[cfg(feature = "hex")]
-impl From<encoding::DecodeError<ScriptBufDecoderError>> for FromHexError {
-    fn from(e: encoding::DecodeError<ScriptBufDecoderError>) -> Self { Self::Decoder(e) }
 }
 
 #[cfg(feature = "arbitrary")]
