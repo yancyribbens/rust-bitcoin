3be528f5fd95c7df8cc48ef478653c1581cea73a
Tobin C. Harding <me@tobin.cc>
2026-03-31 13:04:10 +1100

    hashes: fmt skip a bunch of stuff
    Add attribute to a bunch of stuff recently introduced in #5888.

--- hashes/src/sha256/crypto.rs
+++ hashes/src/sha256/crypto.rs
@@ -835,6 +835,7 @@ impl HashEngine {
         use core::arch::aarch64::vst1q_u8;
 
         // initial state
+        #[rustfmt::skip]
         const INIT: [u32; 8] = [
             0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
             0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
@@ -862,6 +863,7 @@ impl HashEngine {
         ];
 
         // Precomputed W[i] + K[i] for the 2nd transform (padding block).
+        #[rustfmt::skip]
         const MIDS: [u32; 64] = [
             0xc28a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
             0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -900,6 +902,7 @@ impl HashEngine {
         let (mut state0_a, mut state0_b, mut state1_a, mut state1_b);
         let (abcd_save_a, abcd_save_b, efgh_save_a, efgh_save_b);
 
+        #[rustfmt::skip]
         let (mut msg0_a, mut msg0_b, mut msg1_a, mut msg1_b, mut msg2_a, mut msg2_b, mut msg3_a, mut msg3_b);
         let (mut tmp0_a, mut tmp0_b, mut tmp2_a, mut tmp2_b, mut tmp);
 
